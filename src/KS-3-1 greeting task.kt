b7ad8652fbd00c9df26bc061f931c69d3f06372f
mcmakar056 <mcmakar056@xmail.ru>
2026-04-22 22:27:47 +0300

    fix KS-3-1 greeting task

---
+++ src/KS-3-1 greeting task.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var userName = "Валерий"
+    var greeting = "Добрый день!"
+
+    println("${userName}, ${greeting}")
+    greeting = "Добрый вечер!"
+    println("${userName}, ${greeting}")
+}
